a387a23f79e10d96554b93ba11d4cf0b1bfcdc06
Victoria-Ngo-Zi-Nuo <163367489+Victoria-Ngo-Zi-Nuo@users.noreply.github.com>
2024-09-13 08:37:35 +0800

    Update ralat1_calculation.py 1

--- ralat1_calculation.py
+++ ralat1_calculation.py
@@ -2,17 +2,17 @@
 # Betulkan ralat dalam kod Python ini.
 
 def calculation(a, b):    
-    division = c / d
-    return round(divisionn, 2)
+    division = a / b
+    return round(division, 2)
 
 def get_input():
     x = int(input("Masukkan nombor integer pertama:"))
     y = int(input("Masukkan nombor integer kedua:"))
-    return e, f
+    return x, y
 
 def main():
-    (x, y) = get_input(a, b)
-    divide = calculations(x, y)    
+    (x, y) = get_input()
+    divide = calculation(x, y)    
     print(f"Division = {divide}")
 
 if __name__ == "__main__":
